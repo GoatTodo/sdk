3289ac8d1eb475203dd42b8422adf228a8f34d4a
Colton Hurst <colton@coltonhurst.com>
2025-10-15 16:14:32 -0400

    Add UserClient example

--- src/clients/client.rs
+++ src/clients/client.rs
@@ -1,18 +1,20 @@
-use crate::clients::todo::TodoClient;
+use crate::clients::{todo::TodoClient, user::UserClient};
 
 pub struct Client {
-    todos_client: TodoClient,
+    todo_client: TodoClient,
+    user_client: UserClient,
 }
 
 impl Client {
     pub fn new() -> Self {
         Self {
-            todos_client: TodoClient::new(),
+            todo_client: TodoClient::new(),
+            user_client: UserClient::new(),
         }
     }
 
     pub fn todos(&mut self) -> &mut TodoClient {
-        &mut self.todos_client
+        &mut self.todo_client
     }
 }
 
@@ -23,8 +25,11 @@ mod tests {
     #[test]
     fn test() {
         let mut c = Client::new();
-        c.todos().add();
 
+        c.todos().add();
         assert_eq!(String::from("a"), c.todos().get());
+
+        c.todos().add();
+        assert_eq!(String::from("aa"), c.todos().get());
     }
 }
--- src/clients/mod.rs
+++ src/clients/mod.rs
@@ -1,2 +1,3 @@
 pub mod client;
 pub mod todo;
+pub mod user;
